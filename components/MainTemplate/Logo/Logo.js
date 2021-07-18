7abc514c198bb16396fbd1a0b73fa541fda9dcf8
pin-cup <user@mailfence.com>
2021-07-18 16:50:51 +0300

    Add logo for template

---
+++ components/MainTemplate/Logo/Logo.js
@@ -0,0 +1,14 @@
+export default function Logo() {
+  return (
+    <div className="logo-cub">
+      <div className="logo-cub__container">
+        <div className="l-cub__side l-cub__back"></div>
+        <div className="l-cub__side l-cub__left"></div>
+        <div className="l-cub__side l-cub__right"></div>
+        <div className="l-cub__side l-cub__top"></div>
+        <div className="l-cub__side l-cub__bottom"></div>
+        <div className="l-cub__side l-cub__front"></div>
+      </div>
+    </div>
+  )
+}
