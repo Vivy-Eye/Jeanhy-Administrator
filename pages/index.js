a00aebe83efffc84eb9ec73ad063a0904eec579d
pin-cup <user@mailfence.com>
2021-07-18 20:58:08 +0300

    .

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import MainTemplate from '../components/MainTemplate/MainTemplate'
+import Paragraph from '../components/Paragraph'
 
 export default function Home() {
   return (
     <MainTemplate>
-      Some text
+      <Paragraph title="The title" text="Some big text for this paragraph" />
     </MainTemplate>
   )
 }
