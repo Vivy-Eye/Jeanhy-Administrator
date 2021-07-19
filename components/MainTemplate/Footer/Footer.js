8bdb4eac5a5f783cbb5e7db8e0157c5d461869ff
pin-cup <user@mailfence.com>
2021-07-19 17:11:34 +0300

    Add link on github

--- components/MainTemplate/Footer/Footer.js
+++ components/MainTemplate/Footer/Footer.js
@@ -3,7 +3,7 @@ import styles from "../../../styles/Footer.module.scss";
 export default function Footer() {
   return(
     <footer className={styles.footer}> 
-      <p>Powered by Alexander</p>
+      <p>Powered by <a href="https://github.com/pin-cup">Alexander</a></p>
     </footer>
   )
 }
