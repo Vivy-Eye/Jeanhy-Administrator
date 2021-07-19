48fa2b4e82cb8a9bfe4ea2768bd6a4043482686a
pin-cup <user@mailfence.com>
2021-07-19 11:51:29 +0300

    Add themes

---
+++ styles/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+    colors: {
+        black: "#222222",
+        gray:  "#999999",
+        light: "aliceblue",
+    },
+    fonts: {
+        title: '"Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif',
+        menu:  '"Courier New", Courier, monospace',
+    },
+    sizes: {
+        xs: '0',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1440px',
+    }
+};
+
+export default theme;
