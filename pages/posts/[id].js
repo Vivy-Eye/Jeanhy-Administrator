7de97256bd86225ac163be8b0dc51ca4a7cb4b97
pin-cup <user@mailfence.com>
2021-07-19 12:46:12 +0300

    restruct paragraph

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,30 +1,38 @@
 import MainTemplate from "../../components/MainTemplate/MainTemplate";
 import Paragraph from "../../components/Paragraph";
 
-export default function Comments({post, comments}) {
+export default function Comments({ post, comments }) {
   return (
     <MainTemplate headTitle="Post">
-      <Paragraph title={post.title} text={post.body} />
+      <Paragraph title={post.title}>
+        {post.body}
+        <br />
+        <br />
 
-      {comments.map(comment =>
-        <div className="comment" key={post.id}>
-          <Paragraph title={comment.name} text={comment.body}/>
-          <p>From: {comment.email}</p>
-        </div>
-      )}
+        {comments.map((comment) => (
+          <Paragraph key={post.id} title={comment.name}>
+            Comment: {comment.body} <br />
+            From: {comment.email}
+          </Paragraph>
+        ))}
+      </Paragraph>
     </MainTemplate>
-  )
+  );
 }
 
 export async function getServerSideProps(context) {
-  const post = await fetch(`http://jsonplaceholder.typicode.com/posts/${context.params.id}`)
-    .then(response => response.json())
-    .catch(err => console.log(err))
-  const comments = await fetch(`http://jsonplaceholder.typicode.com/posts/${context.params.id}/comments`)
-    .then(response => response.json())
-    .catch(err => console.log(err))
+  const post = await fetch(
+    `http://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+  const comments = await fetch(
+    `http://jsonplaceholder.typicode.com/posts/${context.params.id}/comments`
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
 
   return {
-    props: {post, comments},
-  }
+    props: { post, comments },
+  };
 }
