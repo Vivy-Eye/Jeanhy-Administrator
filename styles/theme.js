7872dda81555bd2d3eb19219e2a58ca79a900486
pin-cup <user@mailfence.com>
2021-07-25 02:19:41 +0300

    Remove sizes add two colors

--- styles/theme.js
+++ styles/theme.js
@@ -6,20 +6,12 @@ const theme = {
         sun: "#e9c46a",
         orange: "#f4a261",
         tomato: "#f4a261",
+        postBlack: "#e8e8e8",
+        underRed: "#483535",
     },
     fonts: {
-        named: '"Verdana", "sans-serif"',
         title: '"Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif',
-        menu:  '"Courier New", Courier, monospace',
     },
-    sizes: {
-        xs: '0',
-        sm: '576px',
-        md: '768px',
-        lg: '992px',
-        xl: '1200px',
-        xxl: '1440px',
-    }
 };
 
 export default theme;
