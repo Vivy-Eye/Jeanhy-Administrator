45c346b84f0476c6d9d85a0e29083359caa37ea6
pin-cup <user@mailfence.com>
2021-07-25 23:16:38 +0300

    Sorted css

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import LoginTemplate from "../components/LoginTemplate/LoginTemplate";
 import Link from "next/link";
 
 const Background = styled.div`
+  align-items: center;
   background-color: ${(props) => props.theme.colors.ocean};
   display: flex;
   justify-content: center;
-  align-items: center;
   min-height: 100vh;
 `;
 
