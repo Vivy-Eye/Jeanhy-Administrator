5c694ea2dce3395adad237e7dfb3e0cff7a72468
pin-cup <user@mailfence.com>
2021-07-25 20:43:36 +0300

    Add login

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-import PageMainTemplate from "../components/PageMainTemplate/PageMainTemplate";
+import styled from "styled-components";
+import LoginTemplate from "../components/LoginTemplate/LoginTemplate";
+import Link from "next/link";
+
+const Background = styled.div`
+  background-color: ${(props) => props.theme.colors.ocean};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`;
 
 export default function Home() {
   return (
-    <PageMainTemplate />
+    <Background>
+      <LoginTemplate />
+      <Link href="/users">To Peges</Link>
+    </Background>
   );
 }
